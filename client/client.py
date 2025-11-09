6dd732364ae6af1c27c8db3faa53b7ccc71e846e
daniy <daniyil.aurora@gmail.com>
2025-11-09 13:50:21 +0100

    basic code

---
+++ client/client.py
@@ -0,0 +1,24 @@
+import socket
+import sys
+
+HEADER = 64
+PORT = 5050
+SERVER = "192.168.10.143"
+ADDR = (SERVER, PORT)
+FORMAT = "utf-8"
+
+DISCONNECT_MSG = "!disconnect"
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(ADDR)
+
+def send(msg: str):
+    message = msg.encode(FORMAT)
+    msg_length = len(message)
+    send_length = str(msg_length).encode(FORMAT)
+    send_length += b' ' * (HEADER - len(send_length))
+    client.send(send_length)
+    client.send(message)
+
+send(' '.join(sys.argv[1:]))
+send("!disconnect")
---
+++ server/server.py
@@ -0,0 +1,42 @@
+import socket
+import threading
+
+HEADER = 64
+PORT = 5050
+SERVER = socket.gethostbyname(socket.gethostname())
+ADDR = (SERVER, PORT)
+FORMAT = "utf-8"
+
+DISCONNECT_MSG = "!disconnect"
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind(ADDR)
+
+def handle_client(conn, addr):
+    print(f"[NEW CONNECTION] {addr} connected.")
+
+    connected = True
+    while connected:
+        msg_length = conn.recv(HEADER).decode(FORMAT)
+        if msg_length:
+            msg_length = int(msg_length)
+            msg = conn.recv(msg_length).decode(FORMAT)
+            print(f"[NEW MESSAGE] [{addr}] {msg}")
+
+            if msg == DISCONNECT_MSG:
+                connected = False
+                print(f"[CONNECTION CLOSED] {addr}")
+            
+    conn.close()
+
+def start():
+    server.listen()
+    print(f"[LISTENING] Server is listening on {SERVER}")
+    while True:
+        conn, addr = server.accept()
+        thread = threading.Thread(target=handle_client, args=(conn, addr))
+        thread.start()
+        print(f"[ACTIVE CONNECTIONS] {threading.active_count() - 1}")
+
+print("[STARTING] Server is starting...")
+start()
